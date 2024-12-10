8df10b9ab4da0622b1c885e6f2c9726b60120468
fazekaszs <zsolt.fazekas.9@gmail.com>
2024-12-10 15:09:42 +0100

    Added a new sub-testcase to the test_small_locohd function (comparison with a hand-calculated value).

--- tests/test_locohd.py
+++ tests/test_locohd.py
@@ -9,11 +9,24 @@ class TestWeightFunctions(unittest.TestCase):
 
         w_func = WeightFunction("uniform", [0., 4.])
         primitive_types = ["O", "A", "B", "C"]
-        sequence = ["O", "A", "B", "C"]
         lchd = LoCoHD(primitive_types, w_func)
+
+        sequence = ["O", "A", "B", "C"]
 
         value = lchd.from_anchors(sequence, sequence, [0., 1., 2., 3.], [0., 1., 1., 1.])
         self.assertAlmostEqual(value, 0.2268, places=4)
 
         value = lchd.from_anchors(sequence, sequence, [0., 1., 1., 1.], [0., 1., 2., 3.])
         self.assertAlmostEqual(value, 0.2268, places=4)
+
+        w_func = WeightFunction("kumaraswamy", [3., 10., 2., 5.])
+        primitive_types = ["A", "B", "C"]
+        lchd = LoCoHD(primitive_types, w_func)
+
+        seq_a = ["A", "B", "A", "C"]
+        dists_a = [0., 1., 5., 9.]
+        seq_b = ["A", "C"]
+        dists_b = [0., 7.]
+
+        value = lchd.from_anchors(seq_a, seq_b, dists_a, dists_b)
+        self.assertAlmostEqual(value, 0.4979, places=4)
