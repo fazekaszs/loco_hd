92a22d0b0c86541a6dd34757db6465c7774b9e31
fazekaszs <zsolt.fazekas.9@gmail.com>
2024-12-13 16:48:58 +0100

    LoCoHD automatically generated value testcases are now saved and loaded into the test_locohd_err function. Additional tests were also added for LoCoHD input validities (reflecting the previous commit). Score distributions are checked against mean, median, score std, min, and max values.

--- tests/generate_locohd_testcases.py
+++ tests/generate_locohd_testcases.py
@@ -1,5 +1,8 @@
 import numpy as np
 import itertools
+import pickle
+from datetime import datetime
+from pathlib import Path
 
 from loco_hd import WeightFunction, LoCoHD, PrimitiveAtom
 
@@ -7,6 +10,8 @@ from loco_hd import WeightFunction, LoCoHD, PrimitiveAtom
 PRIMITIVE_TYPES = [
     "A", "B", "C", "D", "E"
 ]
+THRESHOLD_DISTANCE = 50.
+DATA_DIR = Path("tests/test_data")
 
 
 def generate_wf_params():
@@ -16,14 +21,14 @@ def generate_wf_params():
     weight_functions = list()
 
     # hyper_exp, two parameter type
-    for _ in range(10):
+    for _ in range(5):
         param_b = 1. / np.random.uniform(3., 20.)
         weight_functions.append((
             "hyper_exp", [1., param_b]
         ))
     
     # hyper_exp, six parameter type
-    for _ in range(10):
+    for _ in range(5):
         param_a = np.random.uniform(1E-5, 1., size=3)
         param_b = 1. / np.random.uniform(3., 20., size=3)
         weight_functions.append((
@@ -31,7 +36,7 @@ def generate_wf_params():
         ))
     
     # dagum
-    for _ in range(20):
+    for _ in range(10):
         param_a = np.random.uniform(0.5, 3.0)
         param_b = np.random.uniform(1.5, 4.0)
         param_c = np.random.uniform(1., 25.)
@@ -40,7 +45,7 @@ def generate_wf_params():
         ))
     
     # uniform
-    for _ in range(20):
+    for _ in range(10):
         param_a = np.random.uniform(0.5, 10.0)
         delta_ab = np.random.uniform(0.1, 10.0)
         weight_functions.append((
@@ -48,7 +53,7 @@ def generate_wf_params():
         ))
     
     # kumaraswamy
-    for _ in range(20):
+    for _ in range(10):
         param_a = np.random.uniform(0.5, 10.0)
         delta_ab = np.random.uniform(0.1, 10.0)
         param_c = np.random.uniform(1. + 1E-5, 10.)
@@ -64,7 +69,7 @@ def generate_sequence_coord_pairs():
 
     seq_coord_pairs = list()
 
-    for _ in range(80):
+    for _ in range(40):
 
         seq_len = np.random.randint(50, 301)
         seq = np.random.choice(PRIMITIVE_TYPES, seq_len, replace=True).tolist()
@@ -76,19 +81,50 @@ def generate_sequence_coord_pairs():
 
     return seq_coord_pairs
 
+
 def main():
+
+    timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
     
     wfs = generate_wf_params()
     scps = generate_sequence_coord_pairs()
+
+    with open(DATA_DIR / f"test_input_collection_{timestamp}.pickle", "wb") as f:
+        pickle.dump({
+            "primitive_types": PRIMITIVE_TYPES,
+            "threshold_distance": THRESHOLD_DISTANCE,
+            "weight_functions": wfs,
+            "sequence_coordinate_pairs": scps
+        }, f)
+
+    iterator = itertools.product(
+        enumerate(wfs), 
+        enumerate(scps), 
+        enumerate(scps)
+    )
+
+    collection = dict()
     
-    for (idx_wf, wf), (idx1, scp1), (idx2, scp2) in itertools.product(enumerate(wfs), enumerate(scps), enumerate(scps)):
+    for (idx_wf, wf), (idx1, scp1), (idx2, scp2) in iterator:
+
+        if idx1 == idx2:
+            continue
         
         lchd = LoCoHD(PRIMITIVE_TYPES, WeightFunction(*wf))
         pras1 = [PrimitiveAtom(x, "", y) for x, y in zip(scp1["seq"], scp1["coords"])]
         pras2 = [PrimitiveAtom(x, "", y) for x, y in zip(scp2["seq"], scp2["coords"])]
         anchors = [(x, x) for x in range(min(len(pras1), len(pras2)))]
-        scores = lchd.from_primitives(pras1, pras2, anchors, 50.)
-        # print(idx_wf, idx1, idx2, ":", np.mean(scores), "\n")
+        scores = lchd.from_primitives(pras1, pras2, anchors, THRESHOLD_DISTANCE)
+        collection[(idx_wf, idx1, idx2)] = (
+            np.mean(scores),
+            np.median(scores),
+            np.std(scores),
+            np.min(scores),
+            np.max(scores)
+        )
+
+    with open(DATA_DIR / f"test_output_collection_{timestamp}.pickle", "wb") as f:
+        pickle.dump(collection, f)
 
 
 if __name__ == "__main__":
--- tests/test_locohd.py
+++ tests/test_locohd.py
@@ -1,11 +1,15 @@
-from loco_hd import WeightFunction, LoCoHD, TagPairingRule
+from loco_hd import WeightFunction, LoCoHD, PrimitiveAtom
 
+import numpy as np
 import unittest
+import pickle
+from pathlib import Path
 
 
 class TestLoCoHDValues(unittest.TestCase):
 
     def test_small_locohd(self):
+        """Small LoCoHD calculations using the from_anchors method and hand-calculated test-cases."""
 
         w_func = WeightFunction("uniform", [0., 4.])
         primitive_types = ["O", "A", "B", "C"]
@@ -30,3 +34,60 @@ class TestLoCoHDValues(unittest.TestCase):
 
         value = lchd.from_anchors(seq_a, seq_b, dists_a, dists_b)
         self.assertAlmostEqual(value, 0.4979, places=4)
+
+    def test_locohd_err(self):
+        """Plugging invalid parameters into the LoCoHD constructor."""
+
+        w_func = WeightFunction("uniform", [0., 4.])
+        primitive_types = ["O", "A", "B", "C"]
+
+        with self.assertRaises(ValueError):
+            LoCoHD([], w_func)
+
+        with self.assertRaises(ValueError):
+            LoCoHD(primitive_types, w_func, category_weights=[1., 1., 1.])
+        
+        with self.assertRaises(ValueError):
+            LoCoHD(primitive_types, w_func, category_weights=[1., 1., 1., 1., 1.])
+        
+        with self.assertRaises(ValueError):
+            LoCoHD(primitive_types, w_func, category_weights=[1., -1., 1., 1.])
+        
+        with self.assertRaises(ValueError):
+            LoCoHD(primitive_types, w_func, category_weights=[1., 0., 1., 1.])
+
+    def test_consistency(self):
+        """Large LoCoHD calculations using the from_primitives method and auto-generated test-cases."""
+
+        data_dir = Path("tests/test_data")
+        datafile_date = "241212_131752"
+
+        with open(data_dir / f"test_input_collection_{datafile_date}.pickle", "rb") as f:
+            input_collection = pickle.load(f)
+
+        with open(data_dir / f"test_output_collection_{datafile_date}.pickle", "rb") as f:
+            output_collection = pickle.load(f)
+
+        for idx_wf, idx1, idx2 in output_collection:
+
+            scp1 = input_collection["sequence_coordinate_pairs"][idx1]
+            scp2 = input_collection["sequence_coordinate_pairs"][idx2]
+            pras1 = [PrimitiveAtom(x, "", y) for x, y in zip(scp1["seq"], scp1["coords"])]
+            pras2 = [PrimitiveAtom(x, "", y) for x, y in zip(scp2["seq"], scp2["coords"])]
+            anchors = [(x, x) for x in range(min(len(pras1), len(pras2)))]
+
+            w_func = WeightFunction(*input_collection["weight_functions"][idx_wf])
+            lchd = LoCoHD(input_collection["primitive_types"], w_func)
+            scores = lchd.from_primitives(pras1, pras2, anchors, input_collection["threshold_distance"])
+            current_results = (
+                np.mean(scores),
+                np.median(scores),
+                np.std(scores),
+                np.min(scores),
+                np.max(scores)                
+            )
+
+            target_results = output_collection[(idx_wf, idx1, idx2)]
+
+            for target, current in zip(target_results, current_results):
+                self.assertAlmostEqual(target, current, places=15)
